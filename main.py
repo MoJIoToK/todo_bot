ff6323851b8bb0954eef8f141e8a36a11d40e41b
Nick <mojiotok167@gmail.com>
2024-01-01 22:36:25 +0300

    Add some fix

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-
 HELP = """
 help - напечатать справку по программе.
 add -  добавить задачу в список (название задачи задаёт пользователь).
 show - напечатать все добавленные задачи.
 exit - выход."""
 
-today = []
-tomorrow = []
-other = []
+today = list()
+tomorrow = list()
+other = list()
+tasks = {
+
+}
 
 def add(task):
     date = input("Введите дату: ")
     match date:
-        case "сегодня": today.append(task)
-        case "завтра": tomorrow.append(task)
-        case _: other.append(task)
+        case "сегодня":
+            today.append(task)
+        case "завтра":
+            tomorrow.append(task)
+        case _:
+            other.append(task)
+    pass
+
+
+# def printTask(list):
+#     print(list)
+#     pass
 
 run = True
 
@@ -23,17 +34,25 @@ while run:
     if command == "help":
         print(HELP)
     elif command == "show":
-        print(today)
-        print(tomorrow)
-        print(other)
+        print(tasks)
+        # printTask(today)
+        # print('Завтра: ')
+        # printTask(tomorrow)
+        # print('Другие: ')
+        # printTask(other)
     elif command == "add":
-        task = input("Введите задачу: ")
-        add(task)
+        date = input("Введите дату для добавления задачи: ")
+        task = input("Введите название задачи: ")
+        if date in tasks:
+            tasks[date].append(task)
+        else:
+            tasks[date] = []
+            tasks[date].append(task)
+        print(f"Задача {task} добавлена по дате {date}!")
     elif command == "exit":
         run = False
     else:
         print("Данная команда неизвестна!")
         run = False
-
 
 print("Спасибо за использование! До свидания!")
